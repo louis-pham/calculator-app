d1f5b5e42a2dd6f04eabed811844ab9bffc02862
louis-pham <l.c.pham@hotmail.com>
2019-02-04 17:02:43 +0900

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function CalculatorButton (props) {
+  return (
+    <button className="button" value={props.value}>{props.displayText}</button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      currentValue: 0,
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      currentValue: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+          <input
+            value={this.state.currentValue}
+            onChange={this.handleChange}
+          />
+          <CalculatorButton value="add" displayText="+"/>
+
+          <CalculatorButton value="equals" displayText="="/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <Calculator />
+  </div>,
+  document.querySelector("#root")
+);
+
+const addFunction = (a, b) => a + b;
+const subtractFunction = (a, b) => a - b;
+const multiplyFunction = (a, b) => a * b;
+const divideFunction = (a,b) => a / b;
